c8a5635173e049e8700c2f8ac683b6db1b3ab1b0
Thomas Singer <ts-swt@syntevo.com>
2025-03-11 12:16:03 +0100

    SkijaGC: for Scrollables only use the clientArea as size

--- bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/graphics/SkijaGC.java
+++ bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/graphics/SkijaGC.java
@@ -73,6 +73,9 @@ public class SkijaGC extends GCHandle {
 			var imageBounds = image.getBounds();
 			size.x = imageBounds.width;
 			size.y = imageBounds.height;
+		} else if (drawable instanceof Scrollable scrollable) {
+			final Rectangle clientArea = scrollable.getClientArea();
+			size = new Point(clientArea.width, clientArea.height);
 		} else if (drawable instanceof Control control) {
 			size = control.getSize();
 		} else if (drawable instanceof Device device) {
