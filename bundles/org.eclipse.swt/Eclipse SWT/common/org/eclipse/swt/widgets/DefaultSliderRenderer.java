eba5d4f081cb5172c8f08b1abae36a0abb134d5b
Thomas Singer <ts-swt@syntevo.com>
2025-03-20 11:44:14 +0100

    further drawing fixes

--- bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/widgets/DefaultSliderRenderer.java
+++ bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/widgets/DefaultSliderRenderer.java
@@ -17,7 +17,7 @@ import org.eclipse.swt.graphics.*;
 
 public class DefaultSliderRenderer extends SliderRenderer {
 	private static final int PREFERRED_WIDTH = 170;
-	private static final int PREFERRED_HEIGHT = 42;
+	private static final int PREFERRED_HEIGHT = 10;
 
 	private static final Color DRAG_COLOR = new Color(204, 204, 204);
 	private static final Color LINE_COLOR = new Color(160, 160, 160);
@@ -49,15 +49,13 @@ public class DefaultSliderRenderer extends SliderRenderer {
 			gc.setBackground(background);
 			gc.fillRectangle(0, 0, width, height);
 		}
-		gc.setForeground(LINE_COLOR);
-
-		int trackX, trackY, trackWidth, trackHeight;
+		gc.setBackground(LINE_COLOR);
 
 		if (slider.isVertical()) {
-			trackX = (width - 7) / 2;
-			trackY = 2;
-			trackWidth = 7;
-			trackHeight = height - 4;
+			int trackX = (width - 7) / 2 + 1;
+			int trackY = 2;
+			int trackWidth = 7;
+			int trackHeight = height - 4;
 			trackRectangle = new Rectangle(trackX, trackY, trackWidth, trackHeight);
 
 			int thumbHeight = Math.max(10, (thumb * (height - 4)) / range);
@@ -68,12 +66,12 @@ public class DefaultSliderRenderer extends SliderRenderer {
 			if (!isDragging) {
 				thumbRectangle = new Rectangle(trackX, thumbY, thumbWidth, thumbHeight);
 			}
-			gc.drawLine(0, 0, 0, height);
+			gc.fillRectangle(0, 0, 1, height);
 		} else {
-			trackX = 2;
-			trackY = (height - 7) / 2;
-			trackWidth = width - 4;
-			trackHeight = 7;
+			int trackX = 2;
+			int trackY = (height - 7) / 2 + 1;
+			int trackWidth = width - 4;
+			int trackHeight = 7;
 			trackRectangle = new Rectangle(trackX, trackY, trackWidth, trackHeight);
 
 			int thumbWidth = Math.max(10, (thumb * (width - 4)) / range);
@@ -84,29 +82,27 @@ public class DefaultSliderRenderer extends SliderRenderer {
 			if (!isDragging) {
 				thumbRectangle = new Rectangle(thumbX, trackY, thumbWidth, thumbHeight);
 			}
-			gc.drawLine(0, 0, width, 0);
+			gc.fillRectangle(0, 0, width, 1);
 		}
 
 		// Draw the track
 		if (drawTrack) {
-			gc.setBackground(TRACK_COLOR);
-			gc.drawRoundRectangle(trackRectangle.x, trackRectangle.y, trackRectangle.width, trackRectangle.height, 10,
-					10);
 			gc.fillRoundRectangle(trackRectangle.x, trackRectangle.y, trackRectangle.width, trackRectangle.height, 10,
+					10);
+			gc.setBackground(TRACK_COLOR);
+			gc.fillRoundRectangle(trackRectangle.x + 1, trackRectangle.y + 1, trackRectangle.width - 2, trackRectangle.height - 2, 10,
 					10);
 		}
 
 		// Draw the thumb
+		gc.setBackground(LINE_COLOR);
+		gc.fillRoundRectangle(thumbRectangle.x, thumbRectangle.y, thumbRectangle.width, thumbRectangle.height, 10, 10);
 		if (thumbHovered || isDragging) {
-			gc.setForeground(LINE_COLOR);
 			gc.setBackground(THUMB_HOVER_COLOR);
 		} else {
-			gc.setForeground(LINE_COLOR);
 			gc.setBackground(DRAG_COLOR);
 		}
-		gc.drawRoundRectangle(thumbRectangle.x, thumbRectangle.y, thumbRectangle.width, thumbRectangle.height, 10, 10);
-		gc.fillRoundRectangle(thumbRectangle.x, thumbRectangle.y, thumbRectangle.width, thumbRectangle.height, 10, 10);
-
+		gc.fillRoundRectangle(thumbRectangle.x + 1, thumbRectangle.y + 1, thumbRectangle.width - 2, thumbRectangle.height - 2, 8, 8);
 	}
 
 	@Override
