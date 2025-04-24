273442be3044a800283ee454e36ac0ae2fd4b89f
Thomas Singer <ts-swt@syntevo.com>
2025-04-24 13:47:56 +0200

    Button: changes
    Button: clicking it should set focus
      On GTK it did not became focused.
    Button: fix space handling

--- bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/widgets/Button.java
+++ bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/widgets/Button.java
@@ -310,7 +310,7 @@ public class Button extends CustomControl {
 
 	private void onKeyUp(Event event) {
 		if (event.character == SWT.SPACE) {
-			renderer.setPressed(true);
+			renderer.setPressed(false);
 			handleSelection();
 			redraw();
 		}
@@ -334,6 +334,7 @@ public class Button extends CustomControl {
 	private void onMouseDown(Event e) {
 		if (e.button == 1) {
 			renderer.setPressed(true);
+			setFocus();
 			redraw();
 		}
 	}
